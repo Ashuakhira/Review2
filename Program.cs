0886f2d3817480980249ee5a0c755d04ccf2e0b7
Ashuakhira <bashok592@gmail.com>
2023-05-06 09:29:44 +0530

    Review2 program added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ProgramReview2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Please enter a string");
+            string str = Console.ReadLine();
+            Console.WriteLine("string:" + str);
+            while (str.Length > 0)
+            {
+                Console.Write(str[0] + "=");
+                int cal = 0;
+                for(int j=0;j<str.Length;j++)
+                {
+                    if (str[0] == str[j])
+                    {
+                        cal++;
+                    }
+                }
+                Console.WriteLine(cal);
+                str = str.Replace(str[0].ToString(), string.Empty);  
+            }
+            Console.ReadLine();
+        }
+    }
+}
